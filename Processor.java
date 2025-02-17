e78fc937a8c67e6149cc9a9321c38449f1b6a38c
long-mtl <maithanhlong3806@gmail.com>
2025-02-17 07:46:22 +0700

    Update Processor.java

--- Processor.java
+++ Processor.java
@@ -6,8 +6,8 @@ public class Processor {
     public static void main(String[] args) {
         StudenList studentList = new StudenList();
 
-        Student student1 = new Student("02", "Thanh Long", new Date(2006, 3, 8), "Computer Science", 4.0f);
-        Student student2 = new Student("01", "Thanh Long2", new Date(2006, 3, 3), "Mathematics", 4.0f);
+        Student student1 = new Student("1", "Thanh Long", new Date(2006, 3, 8), "Computer Science", 3.0f);
+        Student student2 = new Student("2", "Thanh Long2", new Date(2006, 3, 3), "Mathematics", 3.0f);
 
         studentList.addStudent(student1);
         studentList.addStudent(student2);
@@ -20,14 +20,14 @@ public class Processor {
         } else {
             System.out.println("Student not found.");
         }
-        boolean editSuccess = studentList.editStudentById("01", "Thanh Long2", new Date(2006, 3, 8), "Computer Science", 4.0f);
+        boolean editSuccess = studentList.editStudentById("1", "Thanh Long2", new Date(2006, 3, 8), "Computer Science", 3.0f);
         if (editSuccess) {
             System.out.println("Student information updated.");
         } else {
             System.out.println("Failed to update student information.");
         }
 
-        boolean deleteSuccess = studentList.deleteStudentById("02");
+        boolean deleteSuccess = studentList.deleteStudentById("2");
         if (deleteSuccess) {
             System.out.println("Student deleted successfully.");
         } else {
